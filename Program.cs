6f513f022a061190e8c287218339cfd989a05bd2
Rojus <rojuslukauskas1@gmail.com>
2024-04-15 20:01:29 +0100

    wrote some tests

--- Program.cs
+++ Program.cs
@@ -1,19 +1,71 @@
 ﻿using ZenithTask.Checkout;
+using System;
 
+namespace Program
+{
+    class Program
+    {
+        public static void Assert(decimal expected, decimal actual)
+        {
+            if (expected == actual)
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("EXPECTED: " + expected + "; ACTUAL: " + actual + ";");
+                Console.WriteLine("Test passed!");
+            }
+            else
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("Test failed!");
+                Console.WriteLine("EXPECTED: " + expected + "; ACTUAL: " + actual + ";");
+            }
+        }
 
-var rule1 = new Rule('A', 2.50m);
-var rule2 = new Rule('B', 2.00m);
-var rule3 = new Rule('A', 7.00m, 3);
+        public static decimal TestPrice(string str)
+        {
+            // test rules (same as in assignment doc)
+            Rule[] rules = 
+                [
+                    new Rule('A', 50.00m),
+                    new Rule('A', 130.00m, 3),
+                    new Rule('B', 30.00m),
+                    new Rule('B', 45.00m, 2),
+                    new Rule('C', 20.00m),
+                    new Rule('D', 15.00m)
+                ];
+            
 
-Rule[] rules = [rule1, rule2, rule3];
+            var checkout = new Checkout(rules);
 
-var checkout = new Checkout(rules);
+            foreach (char c in str)
+            {
+                checkout.Scan(c);
+            }
 
-checkout.Scan('A');
-checkout.Scan('A');
-checkout.Scan('A');
-
-checkout.Scan('B');
-checkout.Scan('2');
-
-checkout.GetTotalPrice();
+            return checkout.GetTotalPrice();
+        }
+        static void Main(string[] args)
+        {
+            // empty string
+            Assert(0, TestPrice(""));
+            // single
+            Assert(50, TestPrice("A"));
+            Assert(30, TestPrice("B"));
+            Assert(20, TestPrice("C"));
+            Assert(15, TestPrice("D"));
+            // offers on their own
+            Assert(130, TestPrice("AAA"));
+            Assert(45, TestPrice("BB"));
+            // multiple offers
+            Assert(260, TestPrice("AAAAAA"));
+            Assert(90, TestPrice("BBBB"));
+            // mixed offers
+            Assert(130 + 45, TestPrice("AAABB"));
+            // mixed offers (non contiguous order)
+            Assert(130 + 45, TestPrice("ABABA"));
+            // offers mixed with non offers
+            Assert((15 * 3) + (20 * 4) + (45 * 2) + 30 + 130 + 100, TestPrice("ADABADCDBCCBABBAC"));
+        
+        }
+    }
+}
